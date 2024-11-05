8ebfb92b93424f7ae6fbccf32968ccfbcea9bf2d
yso7748 <yso7748@nate.com>
2024-11-05 20:32:30 +0900

    config 수정

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,23 @@
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from '../constants/env.js';
+import { CLIENT_VERSION, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER, HOST, PORT } from '../constants/env.js';
+import { PACKET_TYPE_LENGTH, TOTAL_LENGTH } from '../constants/header.js';
 
 export const config = {
+  server: {
+    port: PORT,
+    host: HOST,
+  },
+  client: {
+    version: CLIENT_VERSION,
+  },
+  packet: {
+    totalLength: TOTAL_LENGTH,
+    typeLength: PACKET_TYPE_LENGTH,
+  },
   database: {
     database: DB_NAME,
-    host: DB_HOST,
-    password: DB_PASSWORD,
-    port: DB_PORT,
     user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT,
   },
 };
--- src/constants/env.js
+++ src/constants/env.js
@@ -9,5 +9,5 @@ export const CLIENT_VERSION = process.env.CLIENT_VERSION || '1.0.0';
 export const DB_NAME = process.env.DB_NAME || 'user_db';
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '1234';
-export const DB_HOST = process.env.DB_HOST || '127.0.0.1';
+export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_PORT = process.env.DB_PORT || '3306';
